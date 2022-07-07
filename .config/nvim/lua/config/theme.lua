370b8d05e41c2136520d1178ff4d971e635625f7
jackk <j4ckk3@gmail.com>
2022-07-07 00:57:23 -0500

    changed theme; added alacritty config

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -2,7 +2,7 @@ if vim.fn.exists("+termguicolors") then
   vim.opt.termguicolors = true
 end
 
-vim.g.sonokai_style = "espresso"
+vim.g.sonokai_style = "atlantis"
 vim.g.sonokai_enable_italic = true
 vim.g.sonokai_disable_italic_comment = true
 vim.g.sonokai_transparent_background = true
